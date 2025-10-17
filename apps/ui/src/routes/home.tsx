ebe26233a7ed7585aa94ef60e5f1bca3817bd568
Mohammad Mahdi Bahrami <m.m.bhrm85@gmail.com>
2025-10-17 16:58:41 +0330

    Did some changes...

---
+++ apps/ui/src/routes/home.tsx
@@ -0,0 +1,15 @@
+export function RouteHome() {
+  return (
+    <div className="text-center">
+      <h1 className="text-red-500 font-bold text-3xl underline mb-4">
+        سلام دنیا!
+      </h1>
+
+      <p>چطورین؟</p>
+
+      <pre dir="ltr" lang="en" className="bg-red-200 whitespace-pre-wrap">
+        <code>console.log('Hello World!');</code>
+      </pre>
+    </div>
+  )
+}
